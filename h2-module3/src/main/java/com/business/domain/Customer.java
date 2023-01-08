6aee4d87f76dcf09078a0d73c3d47d828fb1b3a7
zaneerina <zane.erina@gmail.com>
2023-01-08 15:40:27 +0200

    ljoti daudzas rindas - ljoti svarigi infromacijas daudzumi

--- h2-module3/src/main/java/com/business/domain/Customer.java
+++ h2-module3/src/main/java/com/business/domain/Customer.java
@@ -1,6 +1,14 @@
 package com.business.domain;
 public class Customer {
     //the class encapsulates customer data - name, lastname & email
+    /* ljoti svarigas koda rindas ./...
+    asasadsf
+
+    sfdsfdsfgdg
+    dfg
+    dfgfhsf
+    hsfhfshjasdjhsjhfjdsfhjjsfjksjf
+     */
     private final String firstName;
     private final String lastName;
     private final String email;
